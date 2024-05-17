69a6b265d30c05a174832ab52129254a6edf833e
git <git@email.com>
2024-05-17 11:30:51 +0900

    reset debug setup

--- helloworld1.c
+++ helloworld1.c
@@ -3,9 +3,11 @@
 int main()
 {
     printf("Hello, C World!\n");
-    
-    
+
     printf("__STDC_VERSION__ = %ld\n", __STDC_VERSION__);
+
+    int a = 1, b = 2, c = 3;
+    printf("a + b + c = %d\n", a + b + c);
     
     return 0;
 }
